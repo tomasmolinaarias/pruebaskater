0f0997f5e9e00bb60bd084a1ba80ff8e5e751857
victor molina <tomasmolinaarias12@gmail.com>
2022-04-07 20:02:41 -0400

    prueba3

--- database/migracion.sql
+++ database/migracion.sql
@@ -4,7 +4,7 @@ CREATE TABLE skaters (
     id SERIAL PRIMARY KEY, 
     email VARCHAR(50) UNIQUE NOT NULL,
     nombre VARCHAR(25) NOT NULL, 
-    password VARCHAR(50) NOT NULL, 
+    password VARCHAR(60) NOT NULL, 
     anos_experiencia INT NOT NULL,
     especialidad VARCHAR(50) NOT NULL,
     foto VARCHAR(255) NOT NULL, 
